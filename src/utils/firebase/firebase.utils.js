70f82155893e54de48c88cffe7f8b03e74ce2c34
Haleema Mustapha <haleemamustapha@users-MacBook-Air.local>
2024-01-25 11:02:04 +0100

    implemented firebase authentication

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,63 @@
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDUYg5SG0xrFqAod3A-FSUKUT4CqAU111c",
+  authDomain: "pure-wears.firebaseapp.com",
+  projectId: "pure-wears",
+  storageBucket: "pure-wears.appspot.com",
+  messagingSenderId: "88406949984",
+  appId: "1:88406949984:web:09e6914523a705adef0305"
+};
+
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: 'select_account',
+});
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  console.log(userAuth);
+};
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log('error creating the user', error.message);
+    }
+  }
+
+  return userDocRef;
+};
