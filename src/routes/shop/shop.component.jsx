2752c82d94f7e52837f3f80f5d4312064961019e
Haleema Mustapha <haleemamustapha@users-MacBook-Air.local>
2024-02-06 20:30:24 +0100

    product categories preview implemented

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,9 +1,6 @@
 // import { useContext } from 'react';
-
 // import ProductCard from '../../components/product-card/product-card.component';
-
 // import { CategoriesContext } from '../../contexts/categories.context';
-
 // import './shop.styles.scss';
 
 // const Shop = () => {
@@ -11,8 +8,15 @@
 
 //   return (
 //     <div className='products-container'>
-//       {products.map((product) => (
-//         <ProductCard key={product.id} product={product} />
+//       {products && products.map((productGroup, index) => (
+//         <div key={`${productGroup.heading}-${index}`} className='product-group'>
+//           <h2>{productGroup.heading}</h2>
+//           <div className='product-group-items'>
+//             {productGroup.items && productGroup.items.map((product) => (
+//               <ProductCard key={product.id} product={product} />
+//             ))}
+//           </div>
+//         </div>
 //       ))}
 //     </div>
 //   );
@@ -21,7 +25,7 @@
 // export default Shop;
 
 import { useContext } from 'react';
-import ProductCard from '../../components/product-card/product-card.component';
+import CategoryPreview from '../../components/category-preview/category-preview.component';
 import { CategoriesContext } from '../../contexts/categories.context';
 import './shop.styles.scss';
 
@@ -30,16 +34,10 @@ const Shop = () => {
 
   return (
     <div className='products-container'>
-      {products && products.map((productGroup, index) => (
-        <div key={`${productGroup.heading}-${index}`} className='product-group'>
-          <h2>{productGroup.heading}</h2>
-          <div className='product-group-items'>
-            {productGroup.items && productGroup.items.map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-          </div>
-        </div>
-      ))}
+      {products &&
+        products.map((productGroup, index) => (
+          <CategoryPreview key={`${productGroup.heading}-${index}`} productGroup={productGroup} />
+        ))}
     </div>
   );
 };
