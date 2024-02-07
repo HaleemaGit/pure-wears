aa5391e7692baa59aca051eb7f000bbab6933ad8
Haleema Mustapha <haleemamustapha@users-MacBook-Air.local>
2024-02-07 17:44:50 +0100

    fixing deployment

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { CartProvider } from "./contexts/cart.context";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <UserProvider>
         <CategoriesProvider>
           <CartProvider>
