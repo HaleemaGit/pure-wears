c2257d3a7ebe94eabe50f19bb2227b9459a6595f
Haleema Mustapha <haleemamustapha@users-MacBook-Air.local>
2024-03-02 22:40:36 +0100

    incorporating stripe for payment

--- src/index.js
+++ src/index.js
@@ -3,19 +3,23 @@ import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from 'react-redux';
+import { Provider } from "react-redux";
 // import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './store/store';
+import { store, persistor } from "./store/store";
+import { stripePromise } from "./utils/stripe/stripe.utils";
+import { Elements } from "@stripe/react-stripe-js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-  <Provider store={store}>
+    <Provider store={store}>
       {/* <PersistGate loading={null} persistor={persistor}> */}
-    <BrowserRouter basename="/">
-            <App />
-    </BrowserRouter>
-          {/* </PersistGate> */}
+      <BrowserRouter basename="/">
+        <Elements stripe={stripePromise}>
+          <App />
+        </Elements>
+      </BrowserRouter>
+      {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>
 );
